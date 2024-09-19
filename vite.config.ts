7031dddad63791deffc1aa2c04f2ad64f64bf03c
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2024-09-19 13:19:56 +0200

    Fixed error on unused locals and parameters

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  // base: "/first_dapp_frontend/",
+  base: "/ton_sng_dlottery_frontend/",
 })
